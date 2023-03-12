814e6467664931b34306fac8bc1b7340af6e05fb
svetlana_akaemova <akaemova.sv@mail.ru>
2023-03-12 23:34:37 +0100

    fixed db

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -81,12 +81,13 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 if not DEBUG:
     DATABASES['default'] = dj_database_url.config(conn_max_age=500)
