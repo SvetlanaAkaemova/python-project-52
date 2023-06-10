b9dde2e91a6fc5f9494f863f00a802e8d15eb857
svetlana_akaemova <akaemova.sv@mail.ru>
2023-06-10 23:34:54 +0200

    fixed views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth import logout
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 from django.contrib.messages.views import SuccessMessageMixin
@@ -11,9 +9,6 @@ from django.urls import reverse_lazy
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-# def home(request):
-#    return render(request, 'index.html')
-
 
 class UserLoginView(SuccessMessageMixin, LoginView):
     success_message = _('You are logged in')
@@ -22,7 +17,6 @@ class UserLoginView(SuccessMessageMixin, LoginView):
 
 
 class UserLogoutView(SuccessMessageMixin, LogoutView):
-    success_message = _('You are logged out')
     success_url = reverse_lazy('home')
     template_name = 'logout.html'
 
@@ -31,7 +25,3 @@ class UserLogoutView(SuccessMessageMixin, LogoutView):
         messages.info(request, _('You are logged out'))
         return response
 
-# def user_logout(request):
-#    logout(request)
-#    messages.info(request, _('You are logged out'))
-#    return redirect('home')
