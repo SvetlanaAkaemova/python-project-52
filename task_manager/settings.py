462ad959f0fe78510ca9292499c9ebdc06ba0afa
svetlana_akaemova <akaemova.sv@mail.ru>
2023-06-11 01:04:11 +0200

    fix settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -142,7 +142,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 LANGUAGES = (
     ('en-us', _('English')),
